46742ced1d482697b716036c2aafec5c46d0428c
noellimx <noellimx@gmail.com>
2023-02-11 17:23:49 +0800

    add controller logic for login

--- usecase/auth/index.go
+++ usecase/auth/index.go
@@ -3,18 +3,34 @@ package auth
 import (
 	"errors"
 	"vms-be/entities"
+	"vms-be/infra/database"
 	auth "vms-be/usecase/auth/jwt"
 )
 
 type UseCase struct {
+	repos    Repos
+	services Services
 }
 
 type UseCaseInterface interface {
 	Login(fields entities.LoginFields) (auth.JwtString, error)
 }
 
+var ErrLoginFailed error = errors.New("login failed")
+
 func (uc *UseCase) Login(fields entities.LoginFields) (auth.JwtString, error) {
-	return "", errors.New("login failed")
+	username := fields.Username
+	password := fields.Password
+	ok, err := uc.repos.Auth.Login(username, password)
+
+	if err != nil {
+		return "", err
+	}
+
+	if !ok {
+		return "", ErrLoginFailed
+	}
+	return uc.services.GenerateJWTString(username)
 }
 
 type Repos struct {
@@ -22,7 +38,7 @@ type Repos struct {
 }
 
 type AuthRepo interface {
-	Login(username, password string) (bool, error)
+	database.InfraLoginService
 }
 
 type Services struct {
@@ -34,5 +50,5 @@ type Args struct {
 }
 
 func New(args Args) *UseCase {
-	return &UseCase{}
+	return &UseCase{repos: args.Repos}
 }
