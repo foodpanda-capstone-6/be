2fe52aa1de7dbb6062337633cd9b21f640132599
noellimx <67155581+noellimx@users.noreply.github.com>
2023-03-15 21:59:52 +0800

    hacky seed incentive without voucher code

--- schemas/incentives.sql
+++ schemas/incentives.sql
@@ -3,7 +3,18 @@ DROP TABLE IF EXISTS incentives;
 CREATE TABLE IF NOT EXISTS incentives (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     username varchar(64) NOT NULL,
-    incentive_code varchar(10) NOT NULL,
-    transfer_code varchar(64),
+    incentive_code varchar(10) NOT NULL UNIQUE,
+    transfer_code varchar(64) DEFAULT "",
     value int NOT NULL
 );
+INSERT INTO incentives (
+        id,
+        username,
+        incentive_code,
+        transfer_code,
+        value
+    )
+VALUES (1001, "kai", "SAMP001", "", 5),
+    (1002, "naz", "SAMP002", "", 5),
+    (1003, "noel", "SAMP003", "", 5),
+    (1004, "kai", "SAMP004", "", 5);
